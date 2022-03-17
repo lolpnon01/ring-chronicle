9a59244db2f98df39e1af0e4a603b0a6a7f7706d
Julia Semenova <lolpnon01@mail.ru>
2022-03-17 12:29:07 +0300

    updated mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+import { useMedia } from "use-media"
+import classNames from "classnames"
+
+import { mixins } from "utils"
+
+import { ReactComponent as Burger } from "assets/icons/burger.svg"
+import { ReactComponent as Close } from "assets/icons/close.svg"
 import imgLogoSrc from "assets/images/logo.webp"
 import "./Header.scss"
 
 export const Header = (): JSX.Element => {
+  const [sidebar, setSidebar] = useState(false)
+  const isM = useMedia({ maxWidth: mixins.m })
   const links = [
     {
       title: "Marketplace",
@@ -21,8 +31,27 @@ export const Header = (): JSX.Element => {
     },
   ]
 
+  const toggleSidebar = () => {
+    setSidebar(!sidebar)
+  }
+
   return (
     <div className="header">
+      {isM && (
+        <div
+          className={classNames("header-sidebar", {
+            active: sidebar,
+          })}
+        >
+          <div className="header-links">
+            {links.map((link, index) => (
+              <a rel="noreferrer" target="_blank" key={index} href={link.link} className="header-sidebar-link">
+                {link.title}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
       <img src={imgLogoSrc} alt="logo" className="header-img" />
       <div className="header-other">
         {links.map((item, index) => (
@@ -32,6 +61,9 @@ export const Header = (): JSX.Element => {
         ))}
         <button type="button" className="header-other-btn">
           Play Now
+        </button>
+        <button className="header-other-burger" onClick={toggleSidebar}>
+          {sidebar ? <Close /> : <Burger />}
         </button>
       </div>
     </div>
